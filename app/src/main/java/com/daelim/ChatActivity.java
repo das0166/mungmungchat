3fcd6c277054dc028e1d8f070905535df817d724
das0166 <das0166@naver.com>
2022-06-13 18:35:02 +0900

    [2022-06-13] 내가 보낸 채팅 ListView에 구현됨

--- app/src/main/java/com/daelim/ChatActivity.java
+++ app/src/main/java/com/daelim/ChatActivity.java
@@ -6,29 +6,35 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 import java.net.URI;
+import java.util.List;
 
 public class ChatActivity extends AppCompatActivity {
     String id,pw;
     Boolean auto;
     WebSocketClient ws;
-    long mNow;
-    Date mDate;
-    SimpleDateFormat mFormat = new SimpleDateFormat("hh:mm:ss");
-    private String getTime(){
-        mNow = System.currentTimeMillis();
-        mDate = new Date(mNow);
-        return mFormat.format(mDate);
-    }
+    ArrayList<String> list = new ArrayList<String>();
+    ArrayAdapter<String> adapter;
+//    long mNow;
+//    Date mDate;
+//    SimpleDateFormat mFormat = new SimpleDateFormat("hh:mm:ss");
+//    private String getTime(){
+//        mNow = System.currentTimeMillis();
+//        mDate = new Date(mNow);
+//        return mFormat.format(mDate);
+//    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,11 +43,14 @@ public class ChatActivity extends AppCompatActivity {
         TextView getid = findViewById(R.id.id);
         EditText nae = findViewById(R.id.nae);
         ImageButton send = findViewById(R.id.send);
+        ListView listView = findViewById(R.id.naelist);
 
         id=getIntent().getStringExtra("id");
         pw=getIntent().getStringExtra("pw");
         auto=getIntent().getBooleanExtra("auto",true);
 
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,list);
+        listView.setAdapter(adapter);
 
         try {
             ws = new WebSocketClient(new URI("ws://61.83.168.88:4877")) {
@@ -52,13 +61,7 @@ public class ChatActivity extends AppCompatActivity {
                     if(id != null){
                         getid.setText(id + "님 환영합니다.");
                     }
-                    send.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            ws.send("CHAT|"+id+"|"+getTime()+"|"+ nae.getText());
-                            nae.setText(null);
-                        }
-                    });
+
                     out.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View view) {
@@ -71,7 +74,18 @@ public class ChatActivity extends AppCompatActivity {
 
                 @Override
                 public void onMessage(String s) {
+                    send.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            String str = nae.getText().toString();
+                            list.add(str);
+                            adapter.notifyDataSetChanged();
+                            ws.send("CHAT|"+id+"|"+ nae.getText());
+                            nae.setText(null);
+                        }
+                    });
                     Log.e("!!!","onMessage s : "+s);
+
                 }
 
                 @Override
